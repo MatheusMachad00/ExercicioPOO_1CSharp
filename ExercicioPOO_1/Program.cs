12df3cc71aad7b7664ad1d6c668dc0b05105bbcc
mathe <mathe@DESKTOP-F29R11R>
2023-04-13 14:21:06 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioPOO_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using ExercicioPOO_1;
+using System;
+using System.Globalization;
+
+
+//Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.
+
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Pessoa x, y;
+
+            x = new Pessoa();
+            y = new Pessoa();   
+
+            Console.WriteLine("Digite os dados da primeira pessoa");
+            Console.WriteLine("Nome: ");
+            x.name = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            x.age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os dados da segunda pessoa");
+            Console.WriteLine("Nome: ");
+            y.name = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            y.age = int.Parse(Console.ReadLine());
+
+            if (x.age > y.age) Console.WriteLine($"Pessoa mais velha: {x.name}");
+            else if (x.age < y.age) Console.WriteLine($"Pessoa mais velha: {y.name}");
+            else Console.WriteLine("Ambas as pessoas tem a mesma idade.");
+
+        }
+    }
+}
